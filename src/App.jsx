94c5da026635a3438b2874b07d80e027f3bc283a
Kinkin <hiimkien2702@gmail.com>
2024-12-06 23:47:49 -0500

    created Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import './App.css'
+import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
 const App = (() => {
   return (
     <>
-    <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
-      <div className='flixed top-0 -z-10 h-full w-full'></div>
-      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+    <div className="bg-gray-900 text-white p-4">
       <div className='container mx-auto px-8'>
         <Navbar/>
+        <Hero/>
       </div>
     </div>
-
     </>
   )
 })
