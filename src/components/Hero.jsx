b057a446342a2314304c622fc24a3db669a5a64a
Kinkin <hiimkien2702@gmail.com>
2024-12-07 00:13:07 -0500

    create About

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
                                 Kien Nguyen
                             </h1>
                             <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'>
-                                Frontend Developer
+                                Web Developer
                             </span>
                             <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>{HERO_CONTENT}</p>
                         </div>
